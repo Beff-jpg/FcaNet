30f116b71af69c061a9608249cd8e02fa6f67252
beth-brown96 <bethlbrown13@gmail.com>
2024-03-07 10:38:34 +0000

    edited the def setup_seed fucntion to be cpu hard coded

--- main.py
+++ main.py
@@ -168,11 +168,11 @@ class HybridValPipe(Pipeline):
 
 def setup_seed(seed):
      torch.manual_seed(seed)
-     torch.cuda.manual_seed_all(seed)
+     #torch.cuda.manual_seed_all(seed)
      np.random.seed(seed)
      random.seed(seed)
      torch.backends.cudnn.benchmark = False
-     torch.backends.cudnn.deterministic = True
+     #torch.backends.cudnn.deterministic = True
 
 
 def main():
